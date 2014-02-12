ed7105ec2369bc2d9f543927d3a93017aa1bd48c
Jan Zheng <janeazy@gmail.com>
2014-02-12 10:30:22 -0500

    first commit
    everything should be working, though no functional routes (waiting for
    Decatur routes)

---
+++ README.md
@@ -0,0 +1,6 @@
+Let's Run!
+=============
+
+Let's Run is a simple app companion to the Atlanta running group called Chattahoochee Road Runners. We use sheets of paper that show us our routes, water stops, and mileage while we run.
+
+This is just an alternative to these printouts.
---
+++ app/js/project.js
@@ -0,0 +1,19 @@
+
+// $( document ).ready(function() {
+
+//   $('.row').bind("hover", function() {
+//     console.log('yay')
+//   });
+
+
+// });
+
+
+
+$( document ).ready(function() {
+  $( "#map .row" ).bind( "click", function( event ) {
+    // console.log( "The mouse cursor is at (" + event.pageX + ", " + event.pageY + ")" );
+
+    $(this).toggleClass( "done" );
+  });
+});
